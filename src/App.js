6fc57b7bbbb36f499f1a8c84b60255bc5ed0afde
klf006 <49726847+klf006@users.noreply.github.com>
2020-06-29 17:00:30 -0500

    finished components, added react router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import Details from './Components/Details';
+import Cart from './Components/Cart';
+import Default from './Components/Default';
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <h1>hello from app</h1>
+      <ProductList />     
+      <Details />     
+      <Cart />     
+      <Default />         
     </React.Fragment>
   );
 }
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Product extends Component {
   render() {
     return (
       <div>
-        <h3>Hello from product</h3>
+        <h3>Hello from Product</h3>
       </div>
     )
   }
