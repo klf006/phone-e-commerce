87ef72b409e26ec3ccdb8596c292e2da8ab52ab1
klf006 <49726847+klf006@users.noreply.github.com>
2020-06-29 17:10:39 -0500

    added router and switch to app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Navbar';
@@ -11,10 +12,12 @@ function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <ProductList />     
-      <Details />     
-      <Cart />     
-      <Default />         
+      <Switch>
+        <Route exact path='/' component={ProductList} />
+        <Route path='/details' component={Details} />
+        <Route path='/cart' component={Cart} />
+        <Route component={Default} />
+      </Switch>         
     </React.Fragment>
   );
 }
