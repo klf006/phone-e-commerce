a1befb64cbac7d1bfdf86feddc0a500096fb43a3
klf006 <49726847+klf006@users.noreply.github.com>
2020-06-30 13:54:49 -0500

    added logo, links, and classes to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../logo.svg';
+
 
 function NavBar(){
   return(
-    <div>
-      <h1>Hello from Navbar</h1>
-    </div>
+    <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+
+      { /* https://www.iconfinder.com/icons/1243689/call_phone_icon
+           Creative Commons (Attribution 3.0 Unported);
+           https://www.iconfinder.com/Makoto_msk */ }
+
+      <Link to='/'>
+        <img src={Logo} alt='store' className='navbar-brand' />
+      </Link>
+      <ul className='navbar-nav align-items-center'>
+        <li className='nav-item ml-5'>
+          <Link to='/' className='nav-link'>
+            Products
+          </Link>
+        </li>
+      </ul>
+      <Link to='/cart' className='ml-auto'>
+        <button>
+          <i className='fas fa-cart-plus' />
+           <span> My Cart</span>
+        </button>
+      </Link>
+    </nav>
   );
 }
 
