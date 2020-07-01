8fca4aa499e8944fefc2dbe967c4c279457e4914
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-01 15:39:50 -0500

    added button and rules to product card

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -11,10 +11,27 @@ export default class Product extends Component {
     return (
       <ProductWrapper className='col-9 mx-auto col-md-6 col-lg-3 my-3'>
         <div className='card'>        
-          <div className="img-container p-5" onClick={console.log('container click')}>
+          <div className="img-container p-5" onClick={() => console.log('container click')}>
+
             <Link to='/details'>
               <img src={img} alt='product image' className='card-img-top'/>
             </Link>
+            <button 
+              className='cart-btn' 
+              disabled={inCart ? true : false} 
+              onClick={() =>{
+                console.log('added to cart');
+              }}
+            >   
+              {inCart ? (
+                <p className='text-capitalize mb-0' disabled>
+                  {' '}
+                  in inCart
+                </p>
+              ) : (
+                <i className='fas fa-cart-plus' />
+              )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
@@ -26,3 +43,5 @@ const ProductWrapper = styled.div`
 
 
 `;
+
+            
