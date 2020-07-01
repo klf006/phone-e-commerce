0b17e746246156e3f9005a969cb0884684b4e7bd
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-01 15:46:14 -0500

    added card footer , title, and price

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -26,12 +26,22 @@ export default class Product extends Component {
               {inCart ? (
                 <p className='text-capitalize mb-0' disabled>
                   {' '}
-                  in inCart
+                  In Cart
                 </p>
               ) : (
                 <i className='fas fa-cart-plus' />
               )}
             </button>
+          </div>
+          {/* card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className='align-self-center mb-0'>
+              {title}
+            </p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
           </div>
         </div>
       </ProductWrapper>
