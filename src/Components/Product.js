7446b24db9ce8f349341ac19a6bcca257e43fa20
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-01 17:41:18 -0500

    added styled components

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -50,8 +50,24 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-
-
+  .card{
+    border-color: transparent;
+    transition: all 0.5s linear;
+  }
+  .card-footer{
+    background: transparent;
+    border-top: transparent;
+    transition: all 0.5s linear;
+  }
+  &:hover{
+    .card{
+      border: 0.4rem solid rgba(0,0,0,0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.2);
+    }
+    .card-footer{
+      background: rgba(247,247,247);
+    }
+  }
 `;
 
             
