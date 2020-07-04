5fc9980745c4900701a3e9f5a4fefa210e4f44b3
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-04 14:17:22 -0500

    added getItem method

--- src/Context.js
+++ src/Context.js
@@ -26,9 +26,15 @@ class ProductProvider extends Component {
     });
   };
 
+  getItem = (id) =>{
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  }
+
   handleDetail = () =>{
     console.log('hello from detail');
   };
+  
   addToCart = id =>{
     console.log(`hello from add to cart.id is ${id}`);
   };
