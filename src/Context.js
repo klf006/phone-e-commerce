e1d204cb0d8a76322689f3e11f4b5b2ad2b93924
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-07 15:18:57 -0500

    addTotal method and used at bottom of addToCart method

--- src/Context.js
+++ src/Context.js
@@ -9,7 +9,7 @@ class ProductProvider extends Component {
   state={
     products: [],
     detailProduct:detailProduct,
-    cart: storeProducts,
+    cart: [],
     modalOpen: false,
     modalProduct: detailProduct,
     cartSubTotal: 0,
@@ -59,7 +59,7 @@ class ProductProvider extends Component {
       };
     },
       () =>{
-      console.log(this.state);
+      this.addTotals();
       }
     );
   };
@@ -92,6 +92,21 @@ class ProductProvider extends Component {
   clearCart = () =>{
     console.log('cart cleared');
   };
+
+  addTotals = () =>{
+    let subTotal = 0;
+    this.state.cart.map(item =>(subTotal += item.total));
+    const tempTax = subTotal *.0825;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() =>{
+      return {
+        cartSubTotal: subTotal,
+        cartTax: tax,
+        cartTotal: total
+      }
+    });
+  }
 
   render() {
     return (
