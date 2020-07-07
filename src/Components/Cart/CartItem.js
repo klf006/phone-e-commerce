464c0dfe897928ce86e2f2545b43ecf3b64715dd
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-07 14:19:59 -0500

    added cart button css, added content to cart columns

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function CartItem({item, value}) {
   const{id, title, img, price, total, count} = item;
@@ -17,9 +17,18 @@ export default function CartItem({item, value}) {
         <span className='d-lg-none'>product : </span>{title}
       </div>
       <div className="col-10 mx-auto col-lg-2">
-        <span className='d-lg-none'>price : </span>{price}
+        <span className='d-lg-none'>price : </span>${price}
       </div>
-      <div className="col-10 mx-auto col-lg-2"></div>
+      <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+        <div className="d-flex justify-content-center">
+          <div>
+            <span className='btn btn-black mx-1' onClick={() => decrement(id)}>-</span>
+            <span className='btn btn-black mx-1'>{count}</span>
+            <span className='btn btn-black mx-1' onClick={() => increment(id)}>+</span>
+          </div>
+        </div>
+      </div>
+      {/*  */}
       <div className="col-10 mx-auto col-lg-2"></div>
       <div className="col-10 mx-auto col-lg-2"></div>
     </div>
