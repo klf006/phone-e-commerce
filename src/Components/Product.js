c38ecacd53ad464525f4350ab9fea038773e78fd
klf006 <49726847+klf006@users.noreply.github.com>
2020-07-08 14:34:38 -0500

    cleaning

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -22,7 +22,7 @@ export default class Product extends Component {
             }>
 
             <Link to='/details'>
-              <img src={img} alt='product image' className='card-img-top'/>
+              <img src={img} alt='product' className='card-img-top'/>
             </Link>
             <button 
               className='cart-btn' 
